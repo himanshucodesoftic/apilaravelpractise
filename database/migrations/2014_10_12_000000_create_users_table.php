c5378240a5ffad9edf6e4a4c9c62777da515017d
himanshucodesoftic <himanshudhiman@codesoftic.com>
2021-07-23 12:24:15 +0530

    added login logout register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+          
+            $table->string('bussinessname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('c_password');
+            $table->string('countrycode');
+            $table->string('phone');
             $table->rememberToken();
             $table->timestamps();
         });
